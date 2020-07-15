61acd86446accee558d3bb248e0bf51938c47386
david.crespo <david.crespo@hmcts.net>
2020-07-15 21:49:09 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 const sourcePath = path.resolve(__dirname, 'src/main/');
 const govukFrontend = require(path.resolve(__dirname, 'webpack/govukFrontend'));
-const scss = require(path.resolve(__dirname,'webpack/scss'));
-const HtmlWebpack = require(path.resolve(__dirname,'webpack/htmlWebpack'));
+const scss = require(path.resolve(__dirname, 'webpack/scss'));
+const HtmlWebpack = require(path.resolve(__dirname, 'webpack/htmlWebpack'));
 
 module.exports = {
-  plugins: [...govukFrontend.plugins, ...scss.plugins, ...HtmlWebpack.plugins ],
-  entry: path.resolve(sourcePath, 'index.js') ,
+  plugins: [...govukFrontend.plugins, ...scss.plugins, ...HtmlWebpack.plugins],
+  entry: path.resolve(sourcePath, 'index.js'),
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   module: {
     rules: [...scss.rules],
