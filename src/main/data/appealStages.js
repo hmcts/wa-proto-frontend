5c966d5dc7e28103bef11047eadccee5e515c0b5
david.crespo <david.crespo@hmcts.net>
2020-08-11 16:55:09 +0200

    status bar

---
+++ src/main/data/appealStages.js
@@ -0,0 +1,29 @@
+const stages = [
+  {
+    title: 'Start your appeal',
+    active: true,
+    ticked: true,
+  },
+  {
+    title: 'Tell us more',
+    active: true,
+    ticked: false,
+  },
+  {
+    title: 'Prepare for your hearing',
+    active: false,
+    ticked: false,
+  },
+  {
+    title: 'Go to your hearing',
+    active: false,
+    ticked: false,
+  },
+  {
+    title: 'Get your decision',
+    active: false,
+    ticked: false,
+  },
+];
+
+module.exports = stages;
--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,16 +1,17 @@
 import * as express from 'express';
-import { User } from '../models/user';
+const userStages = require('../data/appealStages');
 import Debug from 'debug';
 const debug = Debug('app:home');
 
 const router = express.Router();
 
-const user = new User('David', 'Crespo');
 
 /* GET home page. */
 router.get('/', (req, res) => {
   debug('home router called...');
-  res.render('home', {user: user});
+  res.render('home', {
+    stages: userStages,
+  });
 });
 
 module.exports = router;
