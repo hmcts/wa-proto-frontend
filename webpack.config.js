0a191c1022965e1711c670e0a8f67c2c268738f7
david.crespo <david.crespo@hmcts.net>
2020-08-11 19:31:03 +0200

    fix webpack to move image

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const path = require('path');
 const sourcePath = path.resolve(__dirname, 'src/main/');
 const govukFrontend = require(path.resolve(__dirname, 'webpack/govukFrontend'));
 const scss = require(path.resolve(__dirname, 'webpack/scss'));
+const app = require(path.resolve(__dirname, 'webpack/app'));
 const HtmlWebpack = require(path.resolve(__dirname, 'webpack/htmlWebpack'));
 
 module.exports = {
-  plugins: [...govukFrontend.plugins, ...scss.plugins, ...HtmlWebpack.plugins],
+  plugins: [...govukFrontend.plugins, ...app.plugins, ...scss.plugins, ...HtmlWebpack.plugins],
   entry: path.resolve(sourcePath, 'index.js'),
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   module: {
