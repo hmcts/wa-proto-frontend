8e43d92107dcfe1ee6b63b9bb9c6f0bd9d478eaa
david.crespo <david.crespo@hmcts.net>
2020-08-14 01:37:19 +0200

    fix eslint issue

--- src/main/controllers/home.ts
+++ src/main/controllers/home.ts
@@ -5,7 +5,7 @@ import Debug from 'debug';
 const homePageStages = require('../data/homeAppealStages');
 const debug = Debug('app:controller');
 
-export function createHomePage(req: Request, res: Response) {
+export function createHomePage(req: Request, res: Response): void {
   debug('home controller...');
   res.render('home', {
     stages: new Page('home', homePageStages).stages,
