e2cce2e588598c23fdebcdce979c58958c236734
david.crespo <david.crespo@hmcts.net>
2020-09-02 20:13:35 +0200

    refactor: rename home page and controller

---
+++ src/main/data/stages.js
@@ -0,0 +1,60 @@
+
+const stages = [
+  {
+    title: 'Appeal submitted',
+    active: true,
+    ticked: true,
+  },
+  {
+    title: 'Awaiting respondent evidence',
+    active: true,
+    ticked: true,
+  },
+  {
+    title: 'Case building',
+    active: true,
+    ticked: true,
+  },
+  {
+    title: 'Case under review',
+    active: true,
+    ticked: true,
+  },
+  {
+    title: 'Respondent review',
+    active: true,
+    ticked: false,
+  },
+  {
+    title: 'Submit hearing requirements',
+    active: false,
+    ticked: false,
+  },
+  {
+    title: 'Listing',
+    active: false,
+    ticked: false,
+  },
+  {
+    title: 'Prepare for hearing',
+    active: false,
+    ticked: false,
+  },
+  {
+    title: 'Final bundling',
+    active: false,
+    ticked: false,
+  },
+  {
+    title: 'Pre hearing',
+    active: false,
+    ticked: false,
+  },
+  {
+    title: 'Decision',
+    active: false,
+    ticked: false,
+  },
+];
+
+module.exports = stages;
