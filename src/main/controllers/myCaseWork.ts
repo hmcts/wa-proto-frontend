8e34edb4b5574dccd20767733f72e13ef1dc395d
david.crespo <david.crespo@hmcts.net>
2020-09-04 13:20:17 +0200

    add more tasks to MyTasks and refactor the template

--- src/main/controllers/myCaseWork.ts
+++ src/main/controllers/myCaseWork.ts
@@ -6,6 +6,15 @@ const debug = Debug('app:controller:myCaseWork');
 
 export function createMyCaseWorkPage(req: Request, res: Response): void {
   debug('myCaseWork controller...');
-  const task = new Task('1549 4765 3206 5586', 'Kili Muso', 'Protection', 'Taylor House', 'Review respondent evidence', 'Today');
-  res.render('my-case-work', {task: task});
+  
+  const tasks: Array<Task> = [];
+  const task1 = new Task('1549 4765 3206 5586', 'Kili Muso', 'Protection', 'Taylor House', 'Review respondent evidence', 'Today');
+  const task2 = new Task('1549 5366 1108 0150', 'Mankay Lit', 'Revocation', 'Taylor House', 'Review appellant case', '04 Dec');
+  const task3 = new Task('1549 2345 7854 9786', 'Wina Palic', 'Human Rights', 'Taylor House', 'Review respondent evidence', '+2 days');
+
+  tasks.push(task1);
+  tasks.push(task2);
+  tasks.push(task3);
+
+  res.render('my-case-work', {tasks: tasks});
 }
