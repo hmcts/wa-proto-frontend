1abefe4c3897b405aad876069ff2ad8146422ebb
david.crespo <david.crespo@hmcts.net>
2020-09-09 02:39:12 +0200

    refactor make task easier to read

--- src/main/models/task.ts
+++ src/main/models/task.ts
@@ -3,10 +3,18 @@ import Debug from 'debug';
 const debug = Debug('app:model:task');
 
 export class Task {
-  constructor(public caseRef: string, public caseName: string, public caseCategory: string,
-    public location: string, public taskName: string, public dueDate: string, 
-    public dateOrder: number, public dateColourCode: string) {
+  constructor(
+    public caseRef: string,
+    public caseName: string,
+    public caseCategory: string,
+    public location: string,
+    public taskName: string,
+    public dueDate: string,
+    public dateOrder: number,
+    public dateColourCode: string) {
+
     debug(`task ${caseRef} model created...`);
+
   }
 
 }
