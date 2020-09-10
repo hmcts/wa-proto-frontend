28fe25f77109307aad5ad4f54ee21763c1180d70
david.crespo <david.crespo@hmcts.net>
2020-09-10 17:50:08 +0200

    fix typos

--- src/test/unit/controllers/myCaseWork.test.ts
+++ src/test/unit/controllers/myCaseWork.test.ts
@@ -59,7 +59,7 @@ describe('myCaseWork controller', () => {
   });
   describe('unClaimTask method', () => {
 
-    test('unClaim exsting task', () => {
+    test('unClaim existing task', () => {
 
       const req = (
         {
@@ -87,7 +87,7 @@ describe('myCaseWork controller', () => {
       });
     });
 
-    test('unClaim nonexsting task', () => {
+    test('unClaim non-existing task', () => {
 
       const req = (
         {
