746d65bf1a1f89379a09f0277df1b01892b8d61f
david.crespo <david.crespo@hmcts.net>
2020-09-30 03:19:20 +0200

    refactor code to make simpler

--- src/main/controllers/filterTasksByLocations.ts
+++ src/main/controllers/filterTasksByLocations.ts
@@ -1,26 +1,15 @@
 import { Request, Response } from 'express';
 import Debug from 'debug';
-import { Task } from 'models/task';
 import { MyCaseWorkModel as model } from '../models/myCaseWorkModel';
 
 const filterDebug = Debug('app:controller:filterTasksByLocations');
 
-function filterGivenTaskListByLocations(taskList: Array<Task>, locations: Array<string>): Array<Task> {
-  const newMyAvailableTasks: Array<Task> = [];
-  locations.forEach(location => {
-    const filteredList = taskList.filter(task => task.location === location);
-    newMyAvailableTasks.push(...filteredList);
-  });
-  return newMyAvailableTasks;
-}
-
 export function filterTasksByLocationsController(req: Request, res: Response): void {
   filterDebug('filterTasksByLocations controller...');
   const locations: Array<string> = (Object as any).values(req.query); // eslint-disable-line @typescript-eslint/no-explicit-any
-  filterDebug(`locations: ${JSON.stringify(locations)}`);
 
   req.session.myFilteredAvailableTasks = (locations.length === 0) ? req.session.myAvailableTasks :
-    filterGivenTaskListByLocations(req.session.myAvailableTasks, locations);
+    req.session.myAvailableTasks.filter((x: { location: string }) => locations.includes(x.location));
 
   res.render('my-case-work', {
     tasks: {
