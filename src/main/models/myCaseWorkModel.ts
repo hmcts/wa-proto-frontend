787f46d8ed17ae851f7f70883bee90b01e6df14d
david.crespo <david.crespo@hmcts.net>
2020-10-01 14:44:42 +0200

    refactor: remove unneccessary file

--- src/main/models/myCaseWorkModel.ts
+++ src/main/models/myCaseWorkModel.ts
@@ -1,52 +1,51 @@
-import Debug from 'debug';
-import { tasks } from '../data/myCaseWorkData';
 import { Task } from '../models/task';
 import { Location } from '../models/location';
 
-const debug = Debug('app:model:myCaseWorkModel');
-
 export class MyCaseWorkModel {
 
-  private myAvailableTasks: Array<Task> = [];
-  private myTasks: Array<Task> = [];
-  private myFilteredAvailableTasks: Array<Task> = [];
+  static getMyAvailableTasks(): Array<Task> {
+    const tasks: Array<Task> = [];
 
-  constructor() {
-    debug('MyCaseWorkModel created...');
-    this.buildMyAvailableTasks();
-    this.buildMyTasks();
-    this.buildMyFilteredAvailableTasks();
+    const task0 = new Task('1549 6338 2756 6773', 'Lala Joji', 'Human', 'Taylor House', 'Review respondent evidence', 'Today', 3, 'today');
+    const task1 = new Task('1549 2353 5684 8902', 'Rachel Smith', 'Human', 'Birmingham', 'Review respondent evidence', 'Today', 3, 'today');
+    const task2 = new Task('1549 9061 9513 0004', 'Nonasky Pecki', 'EEA', 'Taylor House', 'Create case summary', '04 Dec', 2, 'future');
+    const task3 = new Task('1549 9083 5480 6960', 'Wauanda Jik', 'Human Rights', 'Taylor House', 'Review appellant case', '+2 days', 4, 'past');
+
+    tasks.push(task0);
+    tasks.push(task1);
+    tasks.push(task2);
+    tasks.push(task3);
+
+    return tasks;
   }
 
-  private buildMyTasks(): void {
-    this.myTasks.push(tasks[4]);
-    this.myTasks.push(tasks[5]);
-    this.myTasks.push(tasks[6]);
+  static getMyFilteredAvailableTasks(): Array<Task> {
+    const tasks: Array<Task> = [];
+
+    const task0 = new Task('1549 6338 2756 6773', 'Lala Joji', 'Human', 'Taylor House', 'Review respondent evidence', 'Today', 3, 'today');
+    const task2 = new Task('1549 9061 9513 0004', 'Nonasky Pecki', 'EEA', 'Taylor House', 'Create case summary', '04 Dec', 2, 'future');
+    const task3 = new Task('1549 9083 5480 6960', 'Wauanda Jik', 'Human Rights', 'Taylor House', 'Review appellant case', '+2 days', 4, 'past');
+
+    tasks.push(task0);
+    tasks.push(task2);
+    tasks.push(task3);
+
+    return tasks;
   }
 
-  private buildMyAvailableTasks(): void {
-    this.myAvailableTasks.push(tasks[0]);
-    this.myAvailableTasks.push(tasks[1]);
-    this.myAvailableTasks.push(tasks[2]);
-    this.myAvailableTasks.push(tasks[3]);
-  }
+  static getMyTasks(): Array<Task> {
+    const tasks: Array<Task> = [];
 
-  private buildMyFilteredAvailableTasks(): void {
-    this.myFilteredAvailableTasks.push(tasks[0]);
-    this.myFilteredAvailableTasks.push(tasks[2]);
-    this.myFilteredAvailableTasks.push(tasks[3]);
-  }
+    const task4 = new Task('1549 4765 3206 5586', 'Kili Muso', 'Protection', 'Taylor House', 'Review respondent evidence', 'Today', 3, 'today');
+    const task5 = new Task('1549 5366 1108 0150', 'Mankay Lit', 'Revocation', 'Taylor House', 'Review appellant case', '14 Dec', 1, 'future');
+    const task6 = new Task('1549 2345 7854 9786', 'Wina Palic', 'Human Rights', 'Taylor House', 'Review respondent evidence', '+4 days', 5, 'past');
 
-  get getMyAvailableTasks(): Array<Task> {
-    return this.myAvailableTasks;
-  }
+    tasks.push(task4);
+    tasks.push(task5);
+    tasks.push(task6);
 
-  get getMyFilteredAvailableTasks(): Array<Task> {
-    return this.myFilteredAvailableTasks;
-  }
+    return tasks;
 
-  get getMyTasks(): Array<Task> {
-    return this.myTasks;
   }
 
   static getAllLocations(): Array<Location> {
