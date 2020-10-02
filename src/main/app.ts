45fc93ea561a64fbb90d3ecf2f62f1ee9ec3af18
lewis150193 <lewis.williams@hmcts.net>
2020-10-02 13:40:44 +0100

    redis-session

--- src/main/app.ts
+++ src/main/app.ts
@@ -7,16 +7,19 @@ import { RouterFinder } from './router/routerFinder';
 import favicon from 'serve-favicon';
 import { HTTPError } from 'HttpError';
 import { Nunjucks } from './modules/nunjucks';
-import session from 'express-session';
 import { MyCaseWorkModel } from './models/myCaseWorkModel';
 import Debug from 'debug';
 import { isNullOrUndefined } from 'util';
 
 const { Express, Logger } = require('@hmcts/nodejs-logging');
 const { setupDev } = require('./development');
+const redis = require('redis');
+const session = require('express-session');
 const env = process.env.NODE_ENV || 'development';
 const developmentMode = env === 'development';
 const debug = Debug('app:app');
+const RedisStore = require('connect-redis')(session);
+const redisClient = redis.createClient();
 
 export const app = express();
 app.locals.ENV = env;
@@ -30,6 +33,7 @@ new Nunjucks(developmentMode).enableFor(app);
 // secure the application by adding various HTTP headers to its responses
 new Helmet(config.get('security')).enableFor(app);
 
+
 app.use(favicon(path.join(__dirname, '/public/assets/images/favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,12 +46,21 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'defaultsecret',
-  cookie: { secure: false },
-}));
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'defaultsecret',
+    resave: false,
+    cookie: { secure: false },
+
+  }),
+);
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: true,
+//   secret: 'defaultsecret',
+//   cookie: { secure: false },
+// }));
 
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   debug(`req.session: ${JSON.stringify(req.session)}`);
--- src/main/server.ts
+++ src/main/server.ts
@@ -7,6 +7,7 @@ import { app } from './app';
 
 const logger = Logger.getLogger('server');
 
+
 // TODO: set the right port for your application
 const port: number = parseInt(process.env.PORT, 10) || 3001;
 
