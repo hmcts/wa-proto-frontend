72486845a2e0f7dddae1e8ddec438b4c8a86a1ce
lewis150193 <lewis.williams@hmcts.net>
2020-10-06 10:04:23 +0100

    task link

--- src/main/controllers/taskListController.ts
+++ src/main/controllers/taskListController.ts
@@ -1,10 +1,20 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import Debug from 'debug';
+import {Task} from '../models/task';
 
 const debug = Debug('app:controller:taskListController');
 
 
 export function createTaskListPage(req: Request, res: Response): void {
+  const removeMyTasksByCase = () => {
+    req.session.myTasks = req.session.myTasks.filter((task: Task) => task.caseRef !== '1549 4765 3206 5586');
+    return res.redirect('/');
+  };
+
+  const redirectTasks = () => {
+    return '/';
+  };
+
   debug('createTaskList controller...');
   res.render('task-list', {
     tasks: {
@@ -12,6 +22,8 @@ export function createTaskListPage(req: Request, res: Response): void {
       'myAvailableTasks': req.session.myFilteredAvailableTasks,
       'addLocations': req.session.addLocations,
       'removeLocations': req.session.removeLocations,
+      removeMyTasksByCase,
+      redirectTasks,
     },
   });
 }
