fcd884aa886ec6c9c9b30231a4c19913f8da154a
lewis150193 <lewis.williams@hmcts.net>
2020-10-06 10:39:04 +0100

    redis-session

---
+++ infrastructure/variables.tf
@@ -0,0 +1,24 @@
+variable "product" {
+  type = "string"
+}
+
+variable "component" {
+  type = "string"
+}
+
+variable "location" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+}
+
+variable "subscription" {
+  type = "string"
+}
+
+variable "common_tags" {
+  type = "map"
+}
--- src/main/app.ts
+++ src/main/app.ts
@@ -7,16 +7,19 @@ import { RouterFinder } from './router/routerFinder';
 import favicon from 'serve-favicon';
 import { HTTPError } from 'HttpError';
 import { Nunjucks } from './modules/nunjucks';
-import session from 'express-session';
 import { MyCaseWorkModel } from './models/myCaseWorkModel';
 import Debug from 'debug';
 import { isNullOrUndefined } from 'util';
 
+const redis = require('redis');
+const session = require('express-session');
 const { Express, Logger } = require('@hmcts/nodejs-logging');
 const { setupDev } = require('./development');
 const env = process.env.NODE_ENV || 'development';
 const developmentMode = env === 'development';
 const debug = Debug('app:app');
+const RedisStore = require('connect-redis')(session);
+const redisClient = redis.createClient();
 
 export const app = express();
 app.locals.ENV = env;
@@ -42,12 +45,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'defaultsecret',
-  cookie: { secure: false },
-}));
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'defaultsecret',
+    resave: false,
+    cookie: { secure: false },
+
+  }),
+);
 
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   debug(`req.session: ${JSON.stringify(req.session)}`);
