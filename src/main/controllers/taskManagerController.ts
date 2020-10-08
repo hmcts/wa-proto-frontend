c26969f9c6f622c881364f113f36e54c10187015
david.crespo <david.crespo@hmcts.net>
2020-10-09 00:31:28 +0200

    add model to the controller to load both dropdown menu values

--- src/main/controllers/taskManagerController.ts
+++ src/main/controllers/taskManagerController.ts
@@ -1,11 +1,51 @@
 import { Request, Response } from 'express';
 import Debug from 'debug';
+import { MyModel } from '../models/myModel';
 
 const debug = Debug('app:controller:taskManagerController');
 
+function getLocationsForDropDownMenu(selectedLocation: string): Array<{}> {
+  return MyModel.getAllLocations().map(l => {
+    if (selectedLocation === l.name) {
+      return {
+        text: l.name,
+        selected: true,
+      };
+    } else {
+      return {
+        text: l.name,
+      };
+    }
+  });
+}
+
+function getCaseWorkersForDropDownMenu(): Array<{}> {
+  return MyModel.getAllCaseworker().map(cw => {
+    return {
+      text: cw.name,
+    };
+  });
+
+}
 
 export function createTaskManagerPage(req: Request, res: Response): void {
   debug('createTaskManagerPage controller...');
-  res.render('task-manager');
+
+  res.render('task-manager', {
+    tasks: {
+      myAvailableTasks: req.session.myAvailableTasks,
+    },
+    locations: getLocationsForDropDownMenu('Taylor House'),
+    caseWorkers: [
+      {
+        text: 'All',
+      },
+      ...getCaseWorkersForDropDownMenu(),
+      {
+        text: 'Unassigned',
+      },
+    ],
+  });
 }
+
 
